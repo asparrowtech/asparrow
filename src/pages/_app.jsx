c96b1753e6c45a52ea20ed7179422b37e8c0222b
asparrowtech <148071624+asparrowtech@users.noreply.github.com>
2023-12-30 15:55:22 +0530

    @vercel/speed-insights/next

--- src/pages/_app.jsx
+++ src/pages/_app.jsx
@@ -1,11 +1,16 @@
 import { Analytics } from '@vercel/analytics/react';
 import '@/src/styles/index.scss'
+import { SpeedInsights } from '@vercel/speed-insights/next';
+
 
 if (typeof window !== "undefined") {
   require("bootstrap/dist/js/bootstrap");
 }
 
 export default function App({ Component, pageProps }) {
-  return <Component {...pageProps} />
+  return <Component {...pageProps} />;
+<Analytics />;
+<SpeedInsights />
 }
-<Analytics />
+
+
--- src/pages/_document.jsx
+++ src/pages/_document.jsx
@@ -1,5 +1,6 @@
 import { Html, Head, Main, NextScript } from 'next/document'
 import { Analytics } from '@vercel/analytics/react';
+import { SpeedInsights } from '@vercel/speed-insights/next';
 
 
 export default function Document() {
@@ -14,6 +15,7 @@ export default function Document() {
       <body>
         <Main />
         <Analytics />
+        <SpeedInsights />
         <NextScript />
       </body>
     </Html>
